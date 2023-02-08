a99803d10e8b83ecf0778aa24352fb19cfc4506b
Anas Temouden <temo27anas@gmail.com>
2023-02-08 20:33:07 +0100

    jh

---
+++ pages/Hello.js
@@ -0,0 +1,12 @@
+const Hello = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <p>Lorem ipsum  sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+        </div>
+
+    );
+    
+}
+
+export default Hello;
